7482b22dd35f83b60e8c9fdbbab0ea4b3eb4ae46
Dharmendra Kumar <kumardakdharmendra@gmail.com>
2022-05-05 16:28:34 +0530

    Button added

--- app/src/main/java/com/dharmendra/intermediatekotlin/adapter/TaskAdapter.kt
+++ app/src/main/java/com/dharmendra/intermediatekotlin/adapter/TaskAdapter.kt
@@ -11,21 +11,51 @@ import com.dharmendra.intermediatekotlin.foundations.BaseRecyclerAdapter
 import com.dharmendra.intermediatekotlin.task.TaskView
 import com.dharmendra.intermediatekotlin.todo.TodoView
 import kotlinx.android.synthetic.main.item_task.view.*
+import kotlinx.android.synthetic.main.view_add_button.view.*
 import kotlinx.android.synthetic.main.view_todo.view.*
 
 class TaskAdapter(taskList:MutableList<Task> = mutableListOf()):
     BaseRecyclerAdapter<Task>(taskList) {
+
+    override fun getItemViewType(position: Int): Int = if (position == 0){
+        TYPE_ADD_BUTTON
+    }else{
+        TYPE_INFO
+    }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int)=
-        MyViewHolder(LayoutInflater.from(parent.context).inflate(
-            R.layout.item_task,
-            parent,false))
+        if (viewType == TYPE_INFO) {
+            MyViewHolder(
+                LayoutInflater.from(parent.context).inflate(
+                    R.layout.item_task,
+                    parent, false
+                )
+            )
+        }else{
+            AddButtonViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.view_add_button,
+                parent,false))
+        }
+
+    override fun getItemCount(): Int {
+        return masterList.size+1
+    }
 
     inner class MyViewHolder(itemView: View):BaseViewHolder<Task>(itemView) {
         override fun onBind(data: Task) {
             (itemView as TaskView).initView(data)
         }
-
     }
+
+    inner class AddButtonViewHolder(view:View):BaseViewHolder<Unit>(view){
+        override fun onBind(data: Unit) {
+            view.buttonText.text = view.context.getString(R.string.add_button_task)
+        }
+    }
+
+    companion object{
+        const val TYPE_ADD_BUTTON = 0
+        const val TYPE_INFO = 1
+    }
+
 
 }
 
--- app/src/main/java/com/dharmendra/intermediatekotlin/foundations/BaseRecyclerAdapter.kt
+++ app/src/main/java/com/dharmendra/intermediatekotlin/foundations/BaseRecyclerAdapter.kt
@@ -2,11 +2,16 @@ package com.dharmendra.intermediatekotlin.foundations
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import com.dharmendra.intermediatekotlin.adapter.TaskAdapter
 
 abstract class BaseRecyclerAdapter<T>(protected val masterList:MutableList<T> = mutableListOf()) :RecyclerView.Adapter<RecyclerView.ViewHolder>(){
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        (holder as BaseViewHolder<T>).onBind(masterList[position])
+        if (holder is TaskAdapter.AddButtonViewHolder){
+            holder.onBind(Unit)
+        }else {
+            (holder as BaseViewHolder<T>).onBind(masterList[position-1])
+        }
     }
 
     override fun getItemCount(): Int = masterList.size
