24777f9840a750330e78b73df4891135c7b4ca1b
Dharmendra Kumar <kumardakdharmendra@gmail.com>
2022-05-06 11:25:25 +0530

    diffutil updated

--- app/src/main/java/com/dharmendra/intermediatekotlin/foundations/BaseRecyclerAdapter.kt
+++ app/src/main/java/com/dharmendra/intermediatekotlin/foundations/BaseRecyclerAdapter.kt
@@ -1,6 +1,7 @@
 package com.dharmendra.intermediatekotlin.foundations
 
 import android.view.View
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.dharmendra.intermediatekotlin.adapter.TaskAdapter
 
@@ -13,10 +14,11 @@ abstract class BaseRecyclerAdapter<T:Any>(protected val masterList:MutableList<T
     }
     override fun getItemCount(): Int = masterList.size
 
-    public fun updateList(list:MutableList<T>){
+    fun updateList(list:MutableList<T>){
+        val result = DiffUtil.calculateDiff(DiffUtilCallbackImpl<T>(masterList,list))
         masterList.clear()
         masterList.addAll(list)
-        notifyDataSetChanged()
+        result.dispatchUpdatesTo(this)
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
@@ -37,5 +39,20 @@ abstract class BaseRecyclerAdapter<T:Any>(protected val masterList:MutableList<T
     companion object{
         const val TYPE_ADD_BUTTON = 0
         const val TYPE_INFO = 1
+    }
+
+    class DiffUtilCallbackImpl<T>(val oldList:MutableList<T>,val newList:MutableList<T>): DiffUtil.Callback() {
+        override fun getOldListSize(): Int = oldList.size
+
+        override fun getNewListSize(): Int = newList.size
+
+        override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+            return oldList[oldItemPosition] == newList[newItemPosition]
+        }
+
+        override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+            return oldList[oldItemPosition] == newList[newItemPosition]
+        }
+
     }
 }
